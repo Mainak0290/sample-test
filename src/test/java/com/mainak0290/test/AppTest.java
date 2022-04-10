648aefd25470e3b68bd6a3c89f945aeebef26f1e
Mainak0290 <mainkskdr@gmail.com>
2022-04-10 21:33:19 +0530

    Added new test

---
+++ src/test/java/com/mainak0290/test/AppTest.java
@@ -0,0 +1,24 @@
+package com.mainak0290.test;
+
+import static org.junit.Assert.assertTrue;
+
+import com.mainak0290.self.App;
+import org.junit.Test;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue1()
+    {
+        assertTrue( App.sum(4,5)==9 );
+
+
+    }
+}
